137bb042fad0f1cbc73622d0d8b59d1764267d9c
Brett Pittman <agbrettpittman@gmail.com>
2023-02-24 21:20:47 -0500

    Playwright tests

--- reactAutomatedTestingExploration/react-puppeteer-2021/src/e2e.test.js
+++ reactAutomatedTestingExploration/react-puppeteer-2021/src/e2e.test.js
@@ -1,24 +1,27 @@
-import puppeteer from "puppeteer";
+import Puppeteer from "puppeteer";
 
 describe("App.js", () => {
-  let browser;
-  let page;
 
-  beforeAll(async () => {
-    browser = await puppeteer.launch();
-    page = await browser.newPage();
-  });
+    let browser;
+    let page;
 
-  it("navigates to the about page", async () => {
-    await page.goto("http://localhost:5000");
-    await page.waitForSelector(".App-welcome-text");
+    beforeAll(async () => {
+        try{
+            browser = await Puppeteer.launch();
+            page = await browser.newPage();
+        } catch(error) {
+            console.log(error)
+        }
+    });
 
-    await page.click("#about-page-link");
-    await page.waitForSelector(".App-welcome-text");
-
-    const text = await page.$eval(".App-welcome-text", (e) => e.textContent);
-    expect(text).toContain("This is the about page.");
-  });
-
-  afterAll(() => browser.close());
+    it("navigates to the about page", async () => {
+        await page.goto("http://localhost:5000");
+        await page.waitForSelector(".App-welcome-text");
+        await page.click("#about-page-link");
+        await page.waitForSelector(".App-welcome-text");
+        const text = await page.$eval(".App-welcome-text", (e) => e.textContent);
+        expect(text).toContain("This is the about page.");
+    });
+    
+    afterAll(async () => browser.close());
 });
